3b9df531c060225202614d4d7e229aa9d90b8d42
kiyaan-coder <kiyaanprozz@gmail.com>
2025-02-19 18:37:24 +0530

    doing html

--- first.py
+++
@@ -1,7 +0,0 @@
-age = int(input("Enter your age : "))
-if age>18 :
-    print("eligible for voting")
-elif age == 18 :
-    print(" eligible for voting")
-else :
-    print("not eligible for voting")
--- problems/ageCalculator.py
+++ problems/ageCalculator.py
@@ -1,1 +1,19 @@
-#calculate the present age with day month and year
+y = int(input("enter your year of bith :"))
+m = int(input("enter your month of bith :"))
+a = int(input("enter your age :"))
+
+if y < 2024:
+  print( 2024-y)
+
+else :
+  print("you arefrom future" ) 
+
+if m  > 0 :
+   print(m + 0) 
+else:
+    print("you are from future")
+
+if a > 0 :
+      print(a + 0)
+else :
+      print("you are from future")

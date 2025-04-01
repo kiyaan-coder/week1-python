3039761959707f62ee2e8f122b9775e8e6b8f036
kiyaan-coder <kiyaanprozz@gmail.com>
2025-04-01 17:37:17 +0530

    problem solved

--- new promblems/circleArea.py
+++ new promblems/circleArea.py
@@ -1,1 +1,6 @@
 #area and circumfeence
+r=int(input("enter the length of radius : "))
+area = 3.14*r*r
+circumfeence=2*3.14*r
+print("the circumfreence is : ",circumfeence)
+print("area is : ",area)

36fa7c1471693a7c84f687b7b7b81f3de767db4f
kiyaan-coder <kiyaanprozz@gmail.com>
2025-04-06 18:18:16 +0530

    problem solved

--- new promblems/calculator.py
+++ new promblems/calculator.py
@@ -1,2 +1,12 @@
 #addition subsraction division multiplication
 
+q=int (input("enter the number : "))
+w=int (input("enter the number : "))
+sum = q+w
+subtract=q-w
+multiply=q*w
+divide=q/w
+print ("the sum is : ",sum)
+print ("the subtraction is : ",subtract)
+print ("the product is : ",multiply)
+print ("the quotient is : ",divide)
